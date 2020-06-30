34eec82d3f8b46e29e4ed1633daa97a695595594
Donald Gray <donald.gray@digirati.com>
2020-06-30 14:06:38 +0100

    Initial apply of 'common'

--- infrastructure/common/terraform.tf
+++ infrastructure/common/terraform.tf
@@ -9,23 +9,6 @@ terraform {
   backend "s3" {
     bucket = "dlcs-remote-state"
     key    = "iiif-builder/common/terraform.tfstate"
-    #dynamodb_table = "terraform-common-locktable"
     region = "eu-west-1"
   }
 }
-
-# resource "aws_dynamodb_table" "terraform-common-locktable" {
-#   name           = "terraform-common-locktable"
-#   read_capacity  = 1
-#   write_capacity = 1
-#   hash_key       = "LockID"
-
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-# }
