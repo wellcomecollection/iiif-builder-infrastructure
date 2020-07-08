c51b1b5af655f62f36acdab4c7addbc27ad660ab
Donald Gray <donald.gray@digirati.com>
2020-07-08 10:47:08 +0100

    Add staging s3 buckets and perms for dashboard
    Moved ECS tasks to their own TF files for clarity

--- infrastructure/staging/s3.tf
+++ infrastructure/staging/s3.tf
@@ -27,3 +27,63 @@ data "aws_iam_policy_document" "wellcomecollection_storage_staging_bucket_read" 
     ]
   }
 }
+
+# Storage Maps
+resource "aws_s3_bucket" "storagemaps" {
+  bucket = "wellcomecollection-stage-iiif-storagemaps"
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "storagemaps_readwrite" {
+  statement {
+    actions = [
+      "s3:*Object",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.storagemaps.arn,
+      "${aws_s3_bucket.storagemaps.arn}/*",
+    ]
+  }
+}
+
+# Presentation
+resource "aws_s3_bucket" "presentation" {
+  bucket = "wellcomecollection-stage-iiif-presentation"
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "presentation_readwrite" {
+  statement {
+    actions = [
+      "s3:*Object",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.presentation.arn,
+      "${aws_s3_bucket.presentation.arn}/*",
+    ]
+  }
+}
+
+# Text
+resource "aws_s3_bucket" "text" {
+  bucket = "wellcomecollection-stage-iiif-text"
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "text_readwrite" {
+  statement {
+    actions = [
+      "s3:*Object",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.text.arn,
+      "${aws_s3_bucket.text.arn}/*",
+    ]
+  }
+}
