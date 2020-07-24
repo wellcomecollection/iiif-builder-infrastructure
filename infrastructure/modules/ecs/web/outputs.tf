b10dd0a6b3e24b498fee4c9f721ae5a1f675b591
Donald Gray <donald.gray@digirati.com>
2020-07-24 14:01:38 +0100

    Tidy listener rules
    Add rules for forwarding from wellcomecollection.org

--- infrastructure/modules/ecs/web/outputs.tf
+++ infrastructure/modules/ecs/web/outputs.tf
@@ -5,3 +5,7 @@ output "task_role_name" {
 output "task_role_arn" {
   value = module.task_definition.task_role_arn
 }
+
+output "service_target_group_arn"{
+  value = aws_alb_target_group.service.arn
+}
