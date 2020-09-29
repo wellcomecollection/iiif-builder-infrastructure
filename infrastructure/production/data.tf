9be655170208dcd028e767adc40822014942e048
Alex Chan <alex@alexwlchan.net>
2020-09-29 08:37:47 +0100

    Use the new location of Digirati VPC tfstate

--- infrastructure/production/data.tf
+++ infrastructure/production/data.tf
@@ -10,13 +10,13 @@ data "terraform_remote_state" "common" {
 }
 
 # remote-state for Wellcome shared platform-infra
-# https://github.com/wellcomecollection/platform-infrastructure/tree/master/critical/back_end
+# https://github.com/wellcomecollection/platform-infrastructure/tree/master/accounts/digirati
 data "terraform_remote_state" "platform_infra" {
   backend = "s3"
 
   config = {
     bucket   = "wellcomecollection-platform-infra"
-    key      = "terraform/platform-infrastructure/shared.tfstate"
+    key      = "terraform/platform-infrastructure/accounts/digirati.tfstate"
     region   = "eu-west-1"
     role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
   }
