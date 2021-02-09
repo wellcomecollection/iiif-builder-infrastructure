7a23aae8afe91340bd8a4ee49f2f1f6f83aa15f6
Donald Gray <donald.gray@digirati.com>
2021-02-09 10:18:04 +0000

    Add cert for wellcomecollection.digirati.io

--- infrastructure/common/variables.tf
+++ infrastructure/common/variables.tf
@@ -1,3 +1,13 @@
 variable "region" {
   default = "eu-west-1"
 }
+
+locals {
+  domain = "wellcomecollection.digirati.io"
+
+  common_tags = {
+    Terraform = true
+    Name      = "iiif-builder",
+    Project   = "iiif-builder"
+  }
+}
