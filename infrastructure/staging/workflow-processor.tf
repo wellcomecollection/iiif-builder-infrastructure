a39798559e1f4b32cc4047185ef43eacea56e90b
tomcrane <tom@nanoplanet.com>
2021-03-16 09:11:54 +0000

    Temporarily increase resources to test Chemist and Druggist

--- infrastructure/staging/workflow-processor.tf
+++ infrastructure/staging/workflow-processor.tf
@@ -8,8 +8,8 @@ module "workflow_processor" {
 
   docker_image = "${data.terraform_remote_state.common.outputs.workflow_processor_url}:staging"
 
-  cpu    = 256
-  memory = 512
+  cpu    = 4096
+  memory = 24576
 
   ecs_cluster_arn                = aws_ecs_cluster.iiif_builder.arn
   service_discovery_namespace_id = data.terraform_remote_state.common.outputs.service_discovery_namespace_id
