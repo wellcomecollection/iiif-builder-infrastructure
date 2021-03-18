4c265f68e846d83186cccdacc347ca426ff4f7a0
tomcrane <tom@nanoplanet.com>
2021-03-18 09:45:37 +0000

    Resources suitable for building C&D on test

--- infrastructure/staging/workflow-processor.tf
+++ infrastructure/staging/workflow-processor.tf
@@ -8,8 +8,8 @@ module "workflow_processor" {
 
   docker_image = "${data.terraform_remote_state.common.outputs.workflow_processor_url}:staging"
 
-  cpu    = 512
-  memory = 2048
+  cpu    = 256
+  memory = 512
 
   ecs_cluster_arn                = aws_ecs_cluster.iiif_builder.arn
   service_discovery_namespace_id = data.terraform_remote_state.common.outputs.service_discovery_namespace_id
