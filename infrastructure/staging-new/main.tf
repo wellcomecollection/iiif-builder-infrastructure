01c9932b1aa93d356c5d4feeec08b8c49de1ce95
Donald Gray <donald.gray@digirati.com>
2023-05-18 14:06:18 +0100

    Remove staging-new resources

--- infrastructure/staging-new/main.tf
+++
@@ -1,9 +0,0 @@
-data "aws_subnet" "private_subnets" {
-  for_each = toset(data.terraform_remote_state.platform_infra.outputs.digirati_vpc_private_subnets)
-  id       = each.value
-}
-
-resource "aws_ecs_cluster" "iiif_builder" {
-  name = local.full_name
-  tags = local.common_tags
-}
