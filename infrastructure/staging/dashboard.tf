8afa1792fcb844e147822d4d9a423603216b720c
Donald Gray <donald.gray@digirati.com>
2024-07-01 13:47:40 +0100

    Explicitly state ASPNETCORE_URLS for stageprod dash
    Required as dotnet8 changes default port

--- infrastructure/staging/dashboard.tf
+++ infrastructure/staging/dashboard.tf
@@ -163,10 +163,11 @@ module "dashboard_stageprod" {
 
   env_vars = {
     "ASPNETCORE_ENVIRONMENT"                    = "Staging-Prod"
+    "ASPNETCORE_URLS"                           = "http://[::]:80"
     "CacheInvalidation__InvalidateIIIFTopicArn" = data.aws_sns_topic.iiif_test_invalidate_cache.arn
     "CacheInvalidation__InvalidateApiTopicArn"  = data.aws_sns_topic.api_stage_invalidate_cache.arn
     "TZ"                                        = "Europe/London"
-    "LANG"                                      = "en_GB.UTF8"
+    "LANG"                                      = "en_GB.UTF8"    
   }
 }
 
