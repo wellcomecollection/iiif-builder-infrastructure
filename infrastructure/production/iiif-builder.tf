8515e0b85f95bb08bc700946a4de09ccc3425402
tomcrane <tom@nanoplanet.com>
2024-08-14 11:10:39 +0100

    add http://*:80 to Prod iiif builder

--- infrastructure/production/iiif-builder.tf
+++ infrastructure/production/iiif-builder.tf
@@ -45,6 +45,8 @@ module "iiif_builder" {
 
   env_vars = {
     "ASPNETCORE_ENVIRONMENT"                  = "Production"
+    "ASPNETCORE_URLS"                         = "http://*:80"
+    "ASPNETCORE_HTTP_PORTS"                   = "80"
     "FeatureManagement__TextServices"         = "False"
     "FeatureManagement__PresentationServices" = "True"
   }
